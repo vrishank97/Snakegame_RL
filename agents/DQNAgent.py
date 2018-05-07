6603650e8c45f7a3d1732776029c64c49cd9c480
Vrishank Bhardwaj <vrishank1997@gmail.com>
2018-05-07 23:36:11 +0530

    Autopep8

--- agents/DQNAgent.py
+++ agents/DQNAgent.py
@@ -1,7 +1,7 @@
-
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+
 
 class DQNAgent:
     def __init__(self):
@@ -13,12 +13,12 @@ class DQNAgent:
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
         self.model = self._build_model()
-        
+
     def _build_model(self):
-      
+
         # Neural Net for Deep-Q learning Model
         model = Sequential()
-        model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(10, 15,3))
+        model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(10, 15, 3))
         model.add(Conv2D(32, (3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
@@ -37,29 +37,29 @@ class DQNAgent:
         model.add(Dense(512, activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(self.action_size, activation='linear'))
-        
+
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
-        
+
         return model
-      
+
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
-        
+
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
+        act_values=self.model.predict(state)
         return np.argmax(act_values[0])  # returns action
-      
+
     def replay(self, batch_size):
-        minibatch = random.sample(self.memory, batch_size)
+        minibatch=random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
-            target = reward
+            target=reward
             if not done:
-              target = reward + self.gamma * \
+              target=reward + self.gamma * \
                        np.amax(self.model.predict(next_state)[0])
-            target_f = self.model.predict(state)
-            target_f[0][action] = target
+            target_f=self.model.predict(state)
+            target_f[0][action]=target
             self.model.fit(state, target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
--- env/snakegame.py
+++ env/snakegame.py
@@ -4,29 +4,29 @@ from collections import deque
 
 
 class SnakeEnv:
-    
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.state = np.zeros((x, y), dtype=int)
         self.reward_range = (0, 1)
-        self.snake = deque([[5,5],[5,6],[5,7],[5,8],[5,9],[5,10]])
+        self.snake = deque([[5, 5], [5, 6], [5, 7], [5, 8], [5, 9], [5, 10]])
         self.score = 0
         self.gameover = 0
         self.reset()
         self.food()
-        
+
     def food(self):
-        a = randint(0,self.x-1)
-        b = randint(0,self.y-1)
+        a = randint(0, self.x-1)
+        b = randint(0, self.y-1)
         if self.state[a][b] == 0:
             self.state[a][b] = 2
         else:
             self.food()
-    
+
     def reset(self):
         self.state = np.zeros((self.x, self.y), dtype=int)
-        self.snake = deque([[5,5],[5,6],[5,7],[5,8],[5,9],[5,10]])
+        self.snake = deque([[5, 5], [5, 6], [5, 7], [5, 8], [5, 9], [5, 10]])
         for i in self.snake:
             self.state[i[0]][i[1]] = 1
         self.state[0] = 3
@@ -34,11 +34,11 @@ class SnakeEnv:
         for i in range(self.x):
             self.state[i][0] = 3
             self.state[i][self.y - 1] = 3
-            
+
     def render(self):
         for i in self.snake:
             self.state[i[0]][i[1]] = 1
-    
+
     def step(self, action):
         state = self.state
         snake = self.snake
@@ -60,7 +60,7 @@ class SnakeEnv:
                 snake.appendleft([head[0], head[1]-1])
                 state[snake[-1][0]][snake[-1][1]] = 0
                 snake.pop()
-    
+
         # Up
         if action == 1:
             # collision with wall
